3823cbdd480d21d090dcd114ee0069e13644425c
Yuichi Kubo <newstart1ine.yk@gmail.com>
2017-09-21 23:33:28 +0000

    add relations to Post.php

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function Comments(){
+        //Post has many comments
+        return $this->hasMany('Comment', 'post_id');
+    }
+    
+    public function Category(){
+        //Post belongs to a Category
+        return $this->belongsTo('Category', 'cat_id');
+    }
 }
