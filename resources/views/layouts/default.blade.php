949a175eea6e6e181a9b8a8d890927e029fd337e
Yuichi Kubo <newstart1ine.yk@gmail.com>
2017-09-22 05:44:03 +0000

    show all posts

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+
+<html lang="ja">
+<head>
+	<meta charset="utf-8" />
+
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+
+	<title>Laravelの掲示板</title>
+</head>
+<body>
+    
+@yield('content')
+
+</body>
+</html>
